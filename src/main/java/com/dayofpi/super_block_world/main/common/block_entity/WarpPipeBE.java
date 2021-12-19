52dd1aefd1b79094cebe02fd0e72b06c91721947
Mathieu Bourgon <matbourgon@gmail.com>
2021-12-18 19:35:42 -0500

    Fixed the world freezing when trying to register a warp pipe into the warp pipe tree

--- src/main/java/com/dayofpi/super_block_world/main/common/block_entity/WarpPipeBE.java
+++ src/main/java/com/dayofpi/super_block_world/main/common/block_entity/WarpPipeBE.java
@@ -8,19 +8,12 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.world.World;
 
 public class WarpPipeBE extends BlockEntity {
     public WarpPipeBE(BlockPos pos, BlockState state) {
         super(BlockEntityRegistry.WARP_PIPE, pos, state);
-    }
-
-    @Override
-    public void setWorld(World world) {
-        super.setWorld(world);
         if (!WarpPipeBlock.warpPipeTree.getChunk(pos.getX() / 16, pos.getZ() / 16).warpList.contains(pos)) {
-            final BlockState blockState = world.getBlockState(pos);
-            if (blockState.isOf(BlockRegistry.WARP_PIPE) && blockState.get(Properties.FACING) == Direction.UP && !blockState.get(Properties.WATERLOGGED))
+            if (state.isOf(BlockRegistry.WARP_PIPE) && state.get(Properties.FACING) == Direction.UP && !state.get(Properties.WATERLOGGED))
                 WarpPipeBlock.warpPipeTree.addBlockToChunk(pos.getX() / 16, pos.getZ() / 16, pos);
         }
     }
